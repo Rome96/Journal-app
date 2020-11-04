1ff74307e8c60efaedad9f3658c1a12a1acef699
Turiano Romero <romero.guzman18@gmail.com>
2020-11-03 22:13:41 -0500

    styles in authRouter

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,11 +1,29 @@
 import React from 'react'
 
 const LoginScreen = () => {
-  return (
-    <div>
-      <h2>Login</h2>
-    </div>
-  )
-}
+  return <div>
+    <h3>Login</h3>
+    <form>
+      <input
+        type="text"
+        placeholder="email@email.com"
+        name="email"
+      />
+      <input
+        type="password"
+        placeholder="*****"
+        name="passsword"
+      />
+      <button
+        type="submit"
 
-export default LoginScreen
+      >
+        Login
+      </button>
+      <hr/>
+      google
+    </form>
+  </div>
+};
+
+export default LoginScreen;
--- src/routers/AuthRouter.js
+++ src/routers/AuthRouter.js
@@ -4,9 +4,13 @@ import LoginScreen from '../components/auth/LoginScreen';
 import RegisterScreen from '../components/auth/RegisterScreen';
 
 export const AuthRouter = () => {
-  return <Switch>
-    <Route exact path="/auth/login" component={ LoginScreen } />
-    <Route exact path="/auth/register" component={ RegisterScreen } />
-    <Redirect to="/auth/login"/>
-  </Switch>
+  return <div className="auth__main">
+    <div className="auth__box-container">
+      <Switch>
+        <Route exact path="/auth/login" component={ LoginScreen } />
+        <Route exact path="/auth/register" component={ RegisterScreen } />
+        <Redirect to="/auth/login"/>
+      </Switch>
+    </div>
+  </div>
 };
