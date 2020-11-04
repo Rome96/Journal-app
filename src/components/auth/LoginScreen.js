e9088ec83750f734170bae68117cd87cc2c5cd7f
Turiano Romero <romero.guzman18@gmail.com>
2020-11-03 23:24:24 -0500

    styles in LoginScreen

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,29 +1,50 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const LoginScreen = () => {
-  return <div>
-    <h3>Login</h3>
-    <form>
-      <input
-        type="text"
-        placeholder="email@email.com"
-        name="email"
-      />
-      <input
-        type="password"
-        placeholder="*****"
-        name="passsword"
-      />
-      <button
-        type="submit"
-
-      >
-        Login
-      </button>
-      <hr/>
-      google
-    </form>
-  </div>
+  return (
+    <div>
+      <h3 className="auth__title mb-5">Login</h3>
+      <form>
+        <input
+          type="text"
+          name="email"
+          autoComplete="off"
+          className="auth__input"
+          placeholder="email@email.com"
+        />
+        <input
+          type="password"
+          name="passsword"
+          className="auth__input"
+          placeholder="*****"
+        />
+        <button
+          type="submit"
+          // disabled
+          className="btn btn-primary mb-1 btn-block"
+        >
+          Login
+        </button>
+        <div className="auth__social-networks">
+          <p>Login with social network</p>
+          <div className="google-btn">
+            <div className="google-icon-wrapper">
+              <img
+                className="google-icon"
+                src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+                alt="google button"
+              />
+            </div>
+            <p className="btn-text">
+              <b>Sign in with google</b>
+            </p>
+          </div>
+        </div>
+        <Link to="/auth/register">Create new account</Link>
+      </form>
+    </div>
+  );
 };
 
 export default LoginScreen;
