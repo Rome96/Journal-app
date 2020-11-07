a449bbb8b985ebcec893c22af0b67245451b6eaf
Turiano Romero <romero.guzman18@gmail.com>
2020-11-06 23:28:20 -0500

    creation of appBar

--- src/components/journal/JournalScreen.js
+++ src/components/journal/JournalScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import NothingSelected from './NothingSelected';
+import NoteScreen from '../notes/NoteScreen';
+// import NothingSelected from './NothingSelected';
 import Sidebar from './Sidebar';
 
 const JournalScreen = () => {
@@ -7,6 +8,7 @@ const JournalScreen = () => {
     <div className="journal__main-content">
       <Sidebar />
       <main>
+        <NoteScreen />
         {/* <NothingSelected /> */}
       </main>
     </div>
---
+++ src/components/notes/NotesAppBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NotesAppBar = () => {
+  return <div className="notes__appbar">
+    <span>6 de noviembre 2020</span>
+    <div>
+      <button className="btn"> Picture </button>
+      <button className="btn"> Save </button>
+    </div>
+  </div>
+};
+
+export default NotesAppBar;
