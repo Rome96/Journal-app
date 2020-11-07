5f8118870269e2ade7faf71771aa06b1f5600d1f
Turiano Romero <romero.guzman18@gmail.com>
2020-11-07 00:11:11 -0500

    create HomeScreen

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -4,6 +4,24 @@ import NotesAppBar from './NotesAppBar';
 const NoteScreen = () => {
   return <div className="notes__main-content">
     <NotesAppBar />
+    <div className="notes__content">
+      <input
+        type="text"
+        autoComplete="off"
+        className="notes__title-input"
+        placeholder="Some awesome title"
+      />
+      <textarea
+        placeholder="What happened today"
+        className="notes__textarea"
+      />
+      <div className="notes__image">
+        <img
+          alt="img"
+          src="https://sm.ign.com/t/ign_latam/screenshot/default/naruto4_st6s.1280.jpg"
+        />
+      </div>
+    </div>
   </div>
 };
 
