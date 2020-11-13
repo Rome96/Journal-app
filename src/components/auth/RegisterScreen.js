c034526ff75e021b538457852a13fcceb586eea9
Turiano Romero <romero.guzman18@gmail.com>
2020-11-13 13:20:49 -0500

    useSelector, return message of error in register form

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import validator from 'validator';
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom';
 import { useForm } from '../../hooks/useForm';
 import { removeError, setError } from '../../redux/actions/ui';
@@ -8,7 +8,8 @@ import { removeError, setError } from '../../redux/actions/ui';
 const RegisterScreen = () => {
 
   const dispatch = useDispatch()
-
+  const { msgError } = useSelector(state => state.ui)
+  console.log(msgError)
   const [formValues, handleInputChange] = useForm({
     name: 'Turianin',
     email: 'rome@rome.com',
@@ -51,9 +52,11 @@ const RegisterScreen = () => {
     <h3 className="auth__title mb-5">Register</h3>
     <form onSubmit={handleSubmit}>
 
-      <div className="auth__alert-error">
-        Error
-      </div>
+      {
+        msgError && <div className="auth__alert-error">
+          { msgError }
+        </div>
+      }
 
       <input
         type="text"
@@ -74,7 +77,7 @@ const RegisterScreen = () => {
       />
       <input
         type="password"
-        name="passsword"
+        name="password"
         value={password}
         placeholder="Password"
         className="auth__input"
